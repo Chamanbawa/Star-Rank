a54fa10c747a1c7072d4af92920439cf1056b94c
chamanbawa <chamanbawa12@gmail.com>
2023-06-07 14:59:05 -0500

    Indentation set

--- src/components/Star.js
+++ src/components/Star.js
@@ -1,14 +1,16 @@
 import { FaStar } from "react-icons/fa";
-import '../style/index.css'
+import "../style/index.css";
 
-function Star({selected = true ,onSelect = f =>f,className}) {
-return(
-        <FaStar className={className}  onClick={onSelect} color={selected ? "#FFD700": "#5c5959"}
-         onMouseEnter={ () => onSelect(true)}
-         onMouseLeave={ () => onSelect(false) }
-         />
-);
-  
+function Star({ selected = true, onSelect = (f) => f, className }) {
+  return (
+    <FaStar
+      className={className}
+      onClick={onSelect}
+      color={selected ? "#FFD700" : "#5c5959"}
+      onMouseEnter={() => onSelect(true)}
+      onMouseLeave={() => onSelect(false)}
+    />
+  );
 }
 
-export default Star
+export default Star;
